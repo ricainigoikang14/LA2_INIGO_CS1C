d14d95d04639b2f79c84df02719551553904316e
ricainigoikang14 <inigorica5@gmail.com>
2025-03-18 17:30:18 +0800

    Create assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,35 @@
+//C1-A1: Variable/Constants Declaration and String Concatenation Methods
+
+// Student's Information
+let myname = "Rica V. Inigo";
+let birthdate = "July 14, 2006";
+let birthplace = "Cadanglaan Banayoyo Ilocos Sur , Philippines 2708";
+let address = "Lintic Banayoyo Ilocos Sur, Philippines 2708";
+let course = "Bachelor of Science in Computer Science";
+let dreamJob = "Software engineering";
+// Converts the students information to Upper Case and Lower Case
+let myname_uppercase = myname.toUpperCase();
+let myname_lowercase = myname.toLowerCase();
+let birthplace_uppercase = birthplace.toUpperCase();
+let address_uppercase = address.toUpperCase();
+let course_lowercase = course.toLowerCase();
+// Classmate 1's Information
+let classmate1Name = "Rhealyn F. Borbe";
+let classmate1Birthdate = "August16, 2006";
+let classmate1Birthplace = "Conconig West, Sta. Lucia, Ilocos Sur, Philippines 2712";
+let classmate1Address = "Conconig West, Sta. Lucia, Ilocos Sur, Philippines 2712";
+let classmate1Course = "Bachelor of Science in Computer Science";
+let classmate1DreamJob = "Web Developer";
+// Converts the classmate 1's information to Upper Case and Lower Case
+let cmate1_uppercase = classmate1Name.toUpperCase();
+let cmate1_lowercase = classmate1Name.toLowerCase();
+let cmate1birthplace_uppercase = classmate1Birthplace.toUpperCase();
+let cmate1address_uppercase =  classmate1Address.toUpperCase();
+let cmate1course_lowercase = classmate1Course.toLowerCase();
+// Classmate 2's Information
+let classmate2Name = "Bretny A. Bal ut";
+let classmate2Birthdate = "May 7, 2006";
+let classmate2Birthplace = "San Manuel, Arzadon Pangasinan Philippines 2438";
+let classmate2Address = "Nangalisan, Sta. Lucia, Ilocos Sur, Philippines 2712";
+let classmate2Course = "Bachelor of Science in Computer Science";
+let classmate2DreamJob = "Software Developer";
